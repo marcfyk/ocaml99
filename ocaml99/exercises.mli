6e430da086802af10a618e693ff0f4c3ae067136
marcfyk <marcjohnfong@gmail.com>
2023-08-12 23:00:31 +0800

    Update docs

--- ocaml99/exercises.mli
+++ ocaml99/exercises.mli
@@ -1,7 +1,8 @@
 (** Tail of a List
     Beginner difficulty
 
-    Write a function last : 'a list -> 'a option that returns the last element of a list. *)
+    Write a function last : 'a list -> 'a option that returns the last element
+    of a list. *)
 val last : 'a list -> 'a option
 
 (** Last Two Elements of a List
@@ -15,7 +16,8 @@ val last_two : 'a list -> ('a * 'a) option
 
     Find the N'th element of a list.
 
-    Remark: OCaml has List.nth which numbers elements from 0 and raises an exception if the index is out of bounds. *)
+    Remark: OCaml has List.nth which numbers elements from 0 and raises an
+    exception if the index is out of bounds. *)
 val nth : 'a list -> int -> 'a option
 
 (** Length of a List
@@ -23,7 +25,8 @@ val nth : 'a list -> int -> 'a option
 
     Find the number of elements of a list.
 
-    OCaml standard library has List.length but we ask that you reimplement it. Bonus for a tail recursive solution. *)
+    OCaml standard library has List.length but we ask that you reimplement it.
+    Bonus for a tail recursive solution. *)
 val length : 'a list -> int
 
 (** Reverse a List
@@ -77,19 +80,27 @@ type 'a rle =
 (** Modified Run-Length Encoding
     Beginner difficulty
 
-    Modify the result of the previous problem in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N E) lists.
+    Modify the result of the previous problem in such a way that if an element
+    has no duplicates it is simply copied into the result list.
+    Only elements with duplicates are transferred as (N E) lists.
 
-    Since OCaml lists are homogeneous, one needs to define a type to hold both single elements and sub-lists. *)
+    Since OCaml lists are homogeneous, one needs to define a type to hold both
+    single elements and sub-lists. *)
 val encode_modified : 'a list -> 'a rle list
 
 (** Decode a Run-Length Encoded List ☡
     Intermediate difficulty
 
-    Given a run-length code list generated as specified in the previous problem, construct its uncompressed version. *)
+    Given a run-length code list generated as specified in the previous problem,
+    construct its uncompressed version. *)
 val decode : 'a rle list -> 'a list
 
 (** Run-Length Encoding of a List (Direct Solution) ☡
     Intermediate difficulty
 
-    Implement the so-called run-length encoding data compression method directly. I.e. don't explicitly create the sublists containing the duplicates, as in problem "Pack consecutive duplicates of list elements into sublists", but only count them. As in problem "Modified run-length encoding", simplify the result list by replacing the singleton lists (1 X) by X. *)
+    Implement the so-called run-length encoding data compression method directly.
+    I.e. don't explicitly create the sublists containing the duplicates,
+    as in problem "Pack consecutive duplicates of list elements into sublists",
+    but only count them. As in problem "Modified run-length encoding",
+    simplify the result list by replacing the singleton lists (1 X) by X. *)
 val encode_direct : 'a list -> 'a rle list
